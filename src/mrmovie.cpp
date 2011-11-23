e50076a559a91adbcd4f2b14f8bf98b922065231
dan paluska <sixmilliondollardan@gmail.com>
2011-11-23 13:44:04 -0500

    twitter functionality, me learning to use git ;)

--- src/mrmovie.cpp
+++ src/mrmovie.cpp
@@ -1,4 +1,5 @@
 #include "mrmovie.h"
+#include "twitcurl.h"
 
 mrmovie::mrmovie(string path)
 {
@@ -8,8 +9,12 @@ mrmovie::mrmovie(string path)
 //--------------------------------------------------------------
 void mrmovie::setup()
 {
+    shortMsg="";
     currentFile=0;
     load();
+    twitSetup();
+   // poststatus();
+
 }
 
 void mrmovie::load()
@@ -78,13 +83,19 @@ void mrmovie::update()
 //--------------------------------------------------------------
 void mrmovie::draw()
 {
-	ofBackground(155,155,155);
-    ofSetColor(ofColor::gray);
+	ofBackground(0,0,0);
+    ofSetColor(ofColor::white);
 
-    movie.draw(20, 20);
+    movie.draw(20, 40);
 
-    ofDrawBitmapString(files[currentFile], 10,10);
+    //ofDrawBitmapString(files[currentFile], 10,10);
+    ofDrawBitmapString(" videos are uploaded daily to http://youtube.com/allasiatwvee", 10,10);
+    ofDrawBitmapString(" <    >  < > <> use the arrrow keys to jump to next/previous movie <> < >  <    >", 10,25);
+    ofDrawBitmapString("type and press enter to send a tweet to http://twitter.com/allasiatwvee",15,580);
+    drawMsg();
+
 }
+
 
 //--------------------------------------------------------------
 void mrmovie::keyPressed  (int key)
@@ -107,4 +118,203 @@ void mrmovie::keyPressed  (int key)
 
             break;
     }
+
+    string s = shortMsg;
+     if((key == 10) ||  (key == 13)){ // RETURN
+    // send the message, clear the messafe
+    poststatus(s);
+    shortMsg="";
+    }
+    else if((key == 8) || (key == 127)) { // BACKSPACE
+        if(s.size() > 0)
+            shortMsg = s.substr(0, s.length() - 1);
+    }
+    else if((key >= 32) && (key <= 255)) { // NORMAL KEY
+        s += (char)key;
+        shortMsg=s;
+    }
+
+
+}
+
+
+void mrmovie::twitSetup()
+{
+
+    XML.loadFile("settings.xml");
+
+ /* Set twitter username and password */
+    string userName = XML.getValue("keys:user","user",0);
+    string passWord = XML.getValue("keys:pass","pass",0);
+
+// cout << userName << "=user";
+    twitterObj.setTwitterUsername( userName );
+    twitterObj.setTwitterPassword( passWord );
+
+    /* OAuth flow begins */
+    /* Step 0: Set OAuth related params. These are got by registering your app at twitter.com */
+    string consumerKey = XML.getValue("keys:ConsumerKey","user",0);
+    string consumerSecret = XML.getValue("keys:ConsumerSecret","pass",0);
+    twitterObj.getOAuth().setConsumerKey( consumerKey );
+    twitterObj.getOAuth().setConsumerSecret( consumerSecret );
+
+    string tokenKey = XML.getValue("keys:TokenKey","user",0);
+    string tokenSecret = XML.getValue("keys:TokenSecret","pass",0);
+    /* Step 1: Check if we alredy have OAuth access token from a previous run */
+    std::string myOAuthAccessTokenKey(tokenKey);
+    std::string myOAuthAccessTokenSecret(tokenSecret);
+    std::ifstream oAuthTokenKeyIn;
+    std::ifstream oAuthTokenSecretIn;
+
+    oAuthTokenKeyIn.open( "twitterClient_token_key.txt" );
+    oAuthTokenSecretIn.open( "twitterClient_token_secret.txt" );
+
+ std::string tmpStr;
+    std::string replyMsg;
+    char tmpBuf[1024];
+    int tmpVar = 0;
+
+    memset( tmpBuf, 0, 1024 );
+    oAuthTokenKeyIn >> tmpBuf;
+    myOAuthAccessTokenKey = tmpBuf;
+
+    memset( tmpBuf, 0, 1024 );
+    oAuthTokenSecretIn >> tmpBuf;
+    myOAuthAccessTokenSecret = tmpBuf;
+
+    oAuthTokenKeyIn.close();
+    oAuthTokenSecretIn.close();
+
+
+    if( myOAuthAccessTokenKey.size() && myOAuthAccessTokenSecret.size() )
+    {
+        /* If we already have these keys, then no need to go through auth again */
+        printf( "\nUsing:\nKey: %s\nSecret: %s\n\n", myOAuthAccessTokenKey.c_str(), myOAuthAccessTokenSecret.c_str() );
+
+        twitterObj.getOAuth().setOAuthTokenKey( myOAuthAccessTokenKey );
+        twitterObj.getOAuth().setOAuthTokenSecret( myOAuthAccessTokenSecret );
+    }
+    else
+    {
+        /* Step 2: Get request token key and secret */
+        twitterObj.oAuthRequestToken( tmpStr );
+
+        /* Step 3: Get PIN  */
+        memset( tmpBuf, 0, 1024 );
+        printf( "\nDo you want to visit twitter.com for PIN (0 for no; 1 for yes): " );
+        gets( tmpBuf );
+        tmpVar = atoi( tmpBuf );
+
+        if( tmpVar > 0 )
+        {
+            /* Ask user to visit twitter.com page and get PIN */
+            memset( tmpBuf, 0, 1024 );
+            printf( "\nPlease visit this link in web browser and authorize this application:\n%s", tmpStr.c_str() );
+            printf( "\nEnter the PIN provided by twitter: " );
+            gets( tmpBuf );
+            tmpStr = tmpBuf;
+            twitterObj.getOAuth().setOAuthPin( tmpStr );
+        }
+        else
+        {
+            /* Else, get it via twitcurl PIN handling */
+            twitterObj.oAuthHandlePIN( tmpStr );
+        }
+
+        /* Step 4: Exchange request token with access token */
+        twitterObj.oAuthAccessToken();
+
+        /* Step 5: Now, save this access token key and secret for future use without PIN */
+        twitterObj.getOAuth().getOAuthTokenKey( myOAuthAccessTokenKey );
+        twitterObj.getOAuth().getOAuthTokenSecret( myOAuthAccessTokenSecret );
+
+        /* Step 6: Save these keys in a file or wherever */
+        std::ofstream oAuthTokenKeyOut;
+        std::ofstream oAuthTokenSecretOut;
+
+        oAuthTokenKeyOut.open( "twitterClient_token_key.txt" );
+        oAuthTokenSecretOut.open( "twitterClient_token_secret.txt" );
+
+        oAuthTokenKeyOut.clear();
+        oAuthTokenSecretOut.clear();
+
+        oAuthTokenKeyOut << myOAuthAccessTokenKey.c_str();
+        oAuthTokenSecretOut << myOAuthAccessTokenSecret.c_str();
+
+        oAuthTokenKeyOut.close();
+        oAuthTokenSecretOut.close();
+    }
+    /* OAuth flow ends */
+
+
+}
+void mrmovie::poststatus()
+{
+     std::string tmpStr;
+    std::string replyMsg;
+       /* Post a new status message */
+    char statusMsg[1024];
+    memset( statusMsg, 0, 1024 );
+    printf( "\nEnter a new status message: " );
+    gets( statusMsg );
+    tmpStr = statusMsg;
+    replyMsg = "";
+    if( twitterObj.statusUpdate( tmpStr ) )
+    {
+        twitterObj.getLastWebResponse( replyMsg );
+        printf( "\ntwitterClient:: twitCurl::statusUpdate web response:\n%s\n", replyMsg.c_str() );
+    }
+    else
+    {
+        twitterObj.getLastCurlError( replyMsg );
+        printf( "\ntwitterClient:: twitCurl::statusUpdate error:\n%s\n", replyMsg.c_str() );
+    }
+
+
+}
+void mrmovie::poststatus(string statusMsg)
+{
+     std::string tmpStr;
+    std::string replyMsg;
+       /* Post a new status message */
+//    char statusMsg[1024];
+  //  memset( statusMsg, 0, 1024 );
+
+    tmpStr = statusMsg;
+    replyMsg = "";
+    if( twitterObj.statusUpdate( tmpStr ) )
+    {
+        twitterObj.getLastWebResponse( replyMsg );
+        printf( "\ntwitterClient:: twitCurl::statusUpdate web response:\n%s\n", replyMsg.c_str() );
+    }
+    else
+    {
+        twitterObj.getLastCurlError( replyMsg );
+        printf( "\ntwitterClient:: twitCurl::statusUpdate error:\n%s\n", replyMsg.c_str() );
+    }
+
+
+}
+
+
+void mrmovie::drawMsg()
+{
+    ofSetColor(ofColor::gray);
+    ofRect(15,585,640,40);
+    ofSetColor(ofColor::white);
+    if (shortMsg.length() > 80)
+    {
+    string s1 = shortMsg.substr(0,80);
+    string s2 = shortMsg.substr(80,shortMsg.length());
+     ofDrawBitmapString(s1,15,600);
+      ofDrawBitmapString(s2,15,615);
+    }
+    else
+    {
+    ofDrawBitmapString(shortMsg,15,600);
+    }
+    int left = 140-shortMsg.length();
+
+    string tmp = "you have " + ofToString(left) + " characters left.";
+    ofDrawBitmapString(tmp,15,640);
 }
--- src/mrmovie.h
+++ src/mrmovie.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "ofMain.h"
+//#include "ofxTwitter.h"
+#include "twitcurl.h"
+#include "ofxXmlSettings.h"
 
 class mrmovie: public ofBaseApp
 {
@@ -11,16 +14,24 @@ class mrmovie: public ofBaseApp
 		void update();
 		void draw();
 
+
 		void keyPressed(int key);
+			void twitSetup();
+			void poststatus();
+			void poststatus(string shortMsg);
+        void drawMsg();
+
+        ofxXmlSettings XML;
 
         string path;
         void load(string path);
         void load();
         vector<string> files;
+        string shortMsg;
 
         int currentFile;
         ofVideoPlayer 		movie;
-
+        twitCurl      twitterObj;
         void next();
         void previous();
         void set(int i);
